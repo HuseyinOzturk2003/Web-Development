062696fc45a4fd2932c05530ee31739db7861cb3
HuseyinOzturk2003 <94167162+HuseyinOzturk2003@users.noreply.github.com>
2022-09-23 16:34:06 +0300

    Update AssemblyInfo.cs

--- WebProject.UI/Properties/AssemblyInfo.cs
+++ WebProject.UI/Properties/AssemblyInfo.cs
@@ -2,9 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Bir derlemeyle ilgili Genel Bilgiler aşağıdaki öznitelik kümesi kullanılarak 
-// denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
-// öznitelik değerlerini değiştirin.
+
 [assembly: AssemblyTitle("WebProject.UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,22 +12,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// ComVisible özniteliğini false olarak ayarlarsanız COM bileşenleri bu 
-// derlemedeki türleri göremez. Bu derlemedeki bir türe COM'dan erişmeniz 
-// gerekiyorsa, o türde ComVisible özniteliğini true olarak ayarlayın.
 [assembly: ComVisible(false)]
 
-// Bu proje COM'a açılmışsa aşağıdaki GUID typelib'in ID'si içindir
 [assembly: Guid("7e93b141-3377-4361-9ba1-c276b7b43d6d")]
 
-// Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
-//
-//      Ana Sürüm
-//      Alt Sürüm 
-//      Yapı Numarası
-//      Düzeltme
-//
-// Tüm değerleri belirtebilirsiniz veya Düzeltme ve Yapı Numaralarına aşağıda 
-// gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
